bb32a14580aa2696ecf50695bb1c07372e1b119a
user <malov08926@gmail.com>
2025-02-19 16:40:53 +0200

    final_final_change_befor_uplowd!!!

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47ac147a7982e02b7329658cb3f8c98e0e4412a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57738b999c32880e1680bcca9cf6cd089f996e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
